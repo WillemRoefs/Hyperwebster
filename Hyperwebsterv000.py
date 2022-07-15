989c3f00517ad39086e5452c21330670de8c51ef
WillemRoefs <109292595+WillemRoefs@users.noreply.github.com>
2022-07-16 00:27:07 +0200

    Now in SQL database

--- Hyperwebsterv000.py
+++ Hyperwebsterv000.py
@@ -2,30 +2,38 @@
 """
 Created on Fri Jul 15 14:53:25 2022
 
-@author: wimme
+@author: Willem Roefs
 """
 
-import numpy as np
-import scipy
-import pandas as pd
+import sqlite3
+import itertools
+# %%
+con = sqlite3.connect('Hyperwebster.db')
+cur = con.cursor()
+cur.execute("DELETE FROM HW")
+# %%
+# cur.execute('''CREATE TABLE HW (ABC)''')
+length = 3
+# ArrABC = []
+ABC = [[' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+       'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']]
 
-Length = 6
-ABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-       'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+for com in itertools.product(*ABC, repeat=length):
+    # print(combination)
+    str1 = str('')
+    for i in com:
+        str1 += i
+    # print(str1.split(' '))
+    if len(str1.split(' ')) >= 2:
+        print(len(str1.split(' ')))
+    # cur.execute("select ABC from HW where ABC=?", (str1,))
+    # data = cur.fetchall()
+    # if not data:
+    if com[-1] != ' ':
+        if len(str1.split(' ')) == 1:
+            cur.execute("INSERT INTO HW VALUES ('" + str1.strip() + "')")    
+    # cur.execute("INSERT INTO HW VALUES ('" + str1 + "')")
 
-totallines = (len(ABC)**Length)
-
-wordlst = []
-
-print(totallines)
-
-lastword1 = np.full(Length, ABC[-1])
-lastword = str()
-for string in lastword1:
-    lastword += string
-
-print(lastword)
-# while (latestword is not lastword):
-#     lastestword = 
-    
-#     wordlist.append()
+# %%
+con.commit()
+con.close()
